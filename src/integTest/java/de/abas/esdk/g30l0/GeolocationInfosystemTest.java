9584e3bad7253a984f5ce0ae87cc7ef96789eaf4
jazz <jasmin.borsig@gmail.com>
2019-02-20 16:06:43 +0100

    #1 basic integration test setup

---
+++ src/integTest/java/de/abas/esdk/g30l0/GeolocationInfosystemTest.java
@@ -0,0 +1,13 @@
+package de.abas.esdk.g30l0;
+
+import de.abas.esdk.test.util.EsdkIntegTest;
+import org.junit.Test;
+
+public class GeolocationInfosystemTest extends EsdkIntegTest {
+
+	@Test
+	public void helloTestingWorld() {
+		ctx.out().println("Hello, Testing World!");
+	}
+
+}
