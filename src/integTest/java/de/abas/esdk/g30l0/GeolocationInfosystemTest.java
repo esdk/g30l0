a355b450ec014ae057a155beccbc4de196a33043
jazz <jasmin.borsig@gmail.com>
2019-02-20 16:42:07 +0100

    #1 Test: can display customer info

--- src/integTest/java/de/abas/esdk/g30l0/GeolocationInfosystemTest.java
+++ src/integTest/java/de/abas/esdk/g30l0/GeolocationInfosystemTest.java
@@ -1,7 +1,16 @@
 package de.abas.esdk.g30l0;
 
 import de.abas.erp.db.DbContext;
+import de.abas.erp.db.Query;
+import de.abas.erp.db.infosystem.custom.ow1.GeoLocation;
+import de.abas.erp.db.schema.customer.Customer;
+import de.abas.erp.db.schema.customer.CustomerEditor;
+import de.abas.erp.db.selection.Conditions;
+import de.abas.erp.db.selection.SelectionBuilder;
 import de.abas.erp.db.util.ContextHelper;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.io.File;
@@ -10,16 +19,57 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
+import static de.abas.esdk.g30l0.GeolocationInfosystemTest.TestData.CUSTOMER;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.hamcrest.core.Is.is;
+
 public class GeolocationInfosystemTest {
 
-	private DbContext ctx = createClientContext();
+	private static DbContext ctx = createClientContext();
 
-	@Test
-	public void helloTestingWorld() {
-		ctx.out().println("Hello, Testing World!");
+	public GeoLocation infosystem = ctx.openInfosystem(GeoLocation.class);
+
+	@BeforeClass
+	public static void createTestData() {
+		CustomerEditor customerEditor = ctx.newObject(CustomerEditor.class);
+		customerEditor.setSwd(CUSTOMER.swd);
+		customerEditor.setZipCode(CUSTOMER.zipCode);
+		customerEditor.setTown(CUSTOMER.town);
+		customerEditor.commit();
 	}
 
-	private DbContext createClientContext() {
+	@Test
+	public void canDisplayCustomerInfo() {
+		infosystem.setCustomersel(CUSTOMER.swd);
+		infosystem.invokeStart();
+
+		assertThat(infosystem.table().getRowCount(), is(1));
+		assertThat(infosystem.table().getRow(1).getCustomer(), is(notNullValue()));
+		assertThat(infosystem.table().getRow(1).getCustomer().getSwd(), is(CUSTOMER.swd));
+		assertThat(infosystem.table().getRow(1).getZipcode(), is(CUSTOMER.zipCode));
+		assertThat(infosystem.table().getRow(1).getTown(), is(CUSTOMER.town));
+	}
+
+	@After
+	public void tidyup() {
+		infosystem.abort();
+	}
+
+	@AfterClass
+	public static void cleanup() {
+		deleteTestData();
+		ctx.close();
+	}
+
+	private static void deleteTestData() {
+		Query<Customer> customers = ctx.createQuery(SelectionBuilder.create(Customer.class).add(Conditions.eq(Customer.META.swd, CUSTOMER.swd)).build());
+		for (final Customer customer : customers) {
+			customer.delete();
+		}
+	}
+
+	private static DbContext createClientContext() {
 		Properties properties = new Properties();
 		File file = new File("gradle.properties");
 		try {
@@ -35,6 +85,21 @@ public class GeolocationInfosystemTest {
 		} catch (IOException e) {
 			throw new RuntimeException("Error while reading file '" + file.getAbsolutePath() + "'.\n" + e.getMessage());
 		}
+	}
+
+	enum TestData {
+		CUSTOMER("G30L0CUS", "67165", "Waldsee");
+
+		String swd;
+		String zipCode;
+		String town;
+
+		TestData(String swd, String zipCode, String town) {
+			this.swd = swd;
+			this.zipCode = zipCode;
+			this.town = town;
+		}
+
 	}
 
 }
