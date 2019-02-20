721683f1d00b67ee922ae11e2ed03a938b17ab9f
jazz <jasmin.borsig@gmail.com>
2019-02-20 22:21:36 +0100

    #2 Implementation: can display trading partner info

--- src/main/java/de/abas/esdk/g30l0/GeolocationInfosystem.java
+++ src/main/java/de/abas/esdk/g30l0/GeolocationInfosystem.java
@@ -5,12 +5,18 @@ import de.abas.erp.axi2.annotation.ButtonEventHandler;
 import de.abas.erp.axi2.annotation.EventHandler;
 import de.abas.erp.axi2.type.ButtonEventType;
 import de.abas.erp.db.DbContext;
-import de.abas.erp.db.Query;
 import de.abas.erp.db.infosystem.custom.ow1.GeoLocation;
 import de.abas.erp.db.schema.customer.Customer;
+import de.abas.erp.db.schema.customer.CustomerContact;
+import de.abas.erp.db.schema.referencetypes.TradingPartner;
+import de.abas.erp.db.schema.vendor.Vendor;
+import de.abas.erp.db.schema.vendor.VendorContact;
 import de.abas.erp.db.selection.Conditions;
 import de.abas.erp.db.selection.SelectionBuilder;
 import de.abas.erp.jfop.rt.api.annotation.RunFopWith;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EventHandler(head = GeoLocation.class, row = GeoLocation.Row.class)
 @RunFopWith(EventHandlerRunner.class)
@@ -18,14 +24,26 @@ public class GeolocationInfosystem {
 
 	@ButtonEventHandler(field = "start", type = ButtonEventType.AFTER)
 	public void startAfter(DbContext ctx, GeoLocation infosystem) {
-		Query<Customer> customers = ctx.createQuery(SelectionBuilder.create(Customer.class).add(Conditions.eq(Customer.META.swd, infosystem.getCustomersel())).build());
-		for (final Customer customer : customers) {
+		for (final TradingPartner tradingPartner : selectTradingPartners(ctx, infosystem.getCustomersel())) {
 			GeoLocation.Row row = infosystem.table().appendRow();
-			row.setCustomer(customer);
-			row.setZipcode(customer.getZipCode());
-			row.setTown(customer.getTown());
-			row.setState(customer.getStateOfTaxOffice());
+			row.setCustomer(tradingPartner);
+			row.setZipcode(tradingPartner.getZipCode());
+			row.setTown(tradingPartner.getTown());
+			row.setState(tradingPartner.getStateOfTaxOffice());
 		}
+	}
+
+	private List<TradingPartner> selectTradingPartners(final DbContext ctx, final String swd) {
+		List<TradingPartner> tradingPartners = new ArrayList<>();
+		tradingPartners.addAll(selectFromTradingPartner(Customer.class, ctx, swd));
+		tradingPartners.addAll(selectFromTradingPartner(CustomerContact.class, ctx, swd));
+		tradingPartners.addAll(selectFromTradingPartner(Vendor.class, ctx, swd));
+		tradingPartners.addAll(selectFromTradingPartner(VendorContact.class, ctx, swd));
+		return tradingPartners;
+	}
+
+	private <T extends TradingPartner> List<T> selectFromTradingPartner(Class<T> clazz, final DbContext ctx, final String swd) {
+		return ctx.createQuery(SelectionBuilder.create(clazz).add(Conditions.eq(T.META.swd, swd)).build()).execute();
 	}
 
 }
